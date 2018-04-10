71c45f7547faced6f65c47b2388d7291404d483d
khoand10@gmail.com <khoand10@gmail.com>
2018-04-10 17:22:06 +0700

    cài đặt Stack

---
+++ src/Stack.java
@@ -0,0 +1,14 @@
+public interface Stack<T> {
+  
+  public boolean empty();
+  
+  public T peek();
+  
+  public T pop();
+
+  public int size();
+  
+  public T push(T obj);
+  
+  public int search(T obj);
+}
